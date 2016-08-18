55bb84e84789472e61e63fee5e8d31169fa8c5e8
olsynt <oleg2807@gmail.com>
2016-08-18 11:18:37 +0300

    Typo fixed

--- startServer.js
+++ startServer.js
@@ -4,12 +4,16 @@ require('shelljs/global');
 var argv = require('minimist')(process.argv.slice(2));
 var branch = argv.branch || 'master';
 
-exec('git pull origin ' + branch, runPm2)
+exec('git pull origin ' + branch, killPm2)
 
-runPm2(){
+function killPm2(){
+	exec('pm2 kill', runPm2)
+}
+
+function runPm2(){
 	exec('pm2 start runServer.sh', savePm2)
 }
 
-savePm2(){
+function savePm2(){
 	exec('pm2 save');
 }
