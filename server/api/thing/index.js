6d490816d230e1753367542c912c9c879e55867c
olsynt <oleg2807@gmail.com>
2016-08-18 16:26:08 +0300

    Added chat as modification of thing

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
+import * as auth from '../../auth/auth.service';
 var express = require('express');
 var controller = require('./thing.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.upsert);
-router.patch('/:id', controller.patch);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.hasRole('user'), controller.index);
+router.get('/:id', auth.hasRole('user'), controller.show);
+router.post('/', auth.hasRole('user'), controller.create);
+router.put('/:id', auth.hasRole('user'), controller.upsert);
+router.patch('/:id', auth.hasRole('user'), controller.patch);
+router.delete('/:id', auth.hasRole('manager'), controller.destroy);
 
 module.exports = router;
