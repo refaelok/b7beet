b451c52230daea452f788a4cc14e63c210cb9f7a
olsynt <oleg2807@gmail.com>
2016-08-21 15:17:46 +0300

    Some more fix

--- startServer.js
+++ startServer.js
@@ -3,12 +3,12 @@
 require('shelljs/global');
 var argv = require('minimist')(process.argv.slice(2));
 var branch = argv.branch || 'master';
-var usenpm = argv.npm || false;
+var usenpm = argv.n || false;
 
 exec('git pull origin ' + branch, npmInstall)
 
 function npmInstall(){
-	usenpm ?  killPm2() : exec('npm install', killPm2) ;
+	usenpm ?  exec('npm install', killPm2) : killPm2() ;
 }
 
 function killPm2(){
