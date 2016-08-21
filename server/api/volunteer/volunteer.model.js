27c0470baa6e09fa7c6edbdd2b2dc3b037b8dd59
olsynt <oleg2807@gmail.com>
2016-08-21 21:38:00 +0300

    Added properties to VolunteerSchema

--- server/api/volunteer/volunteer.model.js
+++ server/api/volunteer/volunteer.model.js
@@ -1,11 +1,28 @@
 'use strict';
 
 import mongoose from 'mongoose';
+var timestamps = require('mongoose-timestamp')
+	,mongooseHistory = require('mongoose-history')
 
 var VolunteerSchema = new mongoose.Schema({
   name: String,
-  info: String,
-  active: Boolean
+  details: String,
+  active: {
+    type: Boolean,
+    default: true
+  },
+	address: mongoose.Schema.Types.Mixed,
+  birthDate: {
+    type: Date,
+    default: undefined
+  },
+  joinDate: {
+    type: Date,
+    default: undefined
+  }
 });
+
+VolunteerSchema.plugin(timestamps);
+VolunteerSchema.plugin(mongooseHistory)
 
 export default mongoose.model('Volunteer', VolunteerSchema);
