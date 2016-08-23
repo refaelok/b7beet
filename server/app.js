fd77c779a8251c6ea607e574bd211f12415819e7
olsynt <oleg2807@gmail.com>
2016-08-23 17:55:33 +0300

    Added more docker testing

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ import config from './config/environment';
 import http from 'http';
 
 // Connect to MongoDB
+console.log("Connectiong to mongo " , config.docker ? config.mongo.containerUri : config.mongo.uri);
 mongoose.connect(config.docker ? config.mongo.containerUri : config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
