17c7e93db6e3f10e188b48993dec85aaa9d72564
olsynt <oleg2807@gmail.com>
2016-09-07 21:55:16 +0300

    More work on event module

--- client/components/eventModule/newEvent/newEvent.component.js
+++ client/components/eventModule/newEvent/newEvent.component.js
@@ -3,9 +3,30 @@
 export class NewEventController{
   constructor(){
     const ctrl = this;
+    ctrl.costumMarker = {
+      lat: undefined,
+      lng: undefined,
+      isShown: false
+    }
   }
 
   test(){
+  }
+
+  openCustomMarker(marker, ctrl){
+    ctrl.costumMarker.lat = marker.latLng.lat();
+    ctrl.costumMarker.lng = marker.latLng.lng();
+    ctrl.costumMarker.isShown = true;
+  }
+
+  closeCustomMarker(marker, ctrl){
+    console.log(ctrl);
+    ctrl.costumMarker.isShown = false;
+  }
+
+  mapClicked(){
+    console.log(this.costumMarker.isShown);
+    this.costumMarker.isShown && (()=> {console.log(123); this.costumMarker.isShown = false;})()
   }
 
 }
